d7ef7bd8defa52e76dfb6eb802827ee8e27382ec
toumi <mtoumi@fia-net.com>
2014-09-17 14:48:25 +0200

    knights project over

--- knights/src/main/java/knightProject/KnightMain.java
+++ knights/src/main/java/knightProject/KnightMain.java
@@ -4,7 +4,7 @@ package knightProject;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class mainTest {
+public class KnightMain {
 
     public static void main(String[] args)
     {
